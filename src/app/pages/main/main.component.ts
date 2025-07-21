ba7c08495e0eac446d5b5088dec2ed8a443a33cb
Ikenna Odoh <ikennaodoh@Ikennas-MacBook-Air-2.local>
2025-07-21 11:42:54 +0100

    layout and menus

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -4,6 +4,7 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { SharedModule } from '../../shared/shared.module';
 
 @Component({
@@ -16,6 +17,7 @@ import { SharedModule } from '../../shared/shared.module';
     NzMenuModule,
     SharedModule,
     NzMenuModule,
+    NzDividerModule
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
