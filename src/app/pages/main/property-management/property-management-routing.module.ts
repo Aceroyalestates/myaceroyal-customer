20c42a792edd3201dff46409413acf1af1a159ea
Ikenna Odoh <ikennaodoh@Ikennas-MacBook-Air-2.local>
2025-07-22 10:16:22 +0100

    property details

--- src/app/pages/main/property-management/property-management-routing.module.ts
+++ src/app/pages/main/property-management/property-management-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PropertyManagementComponent } from './property-management.component';
-import { ViewPropertyComponent } from './view-property/view-property.component';
+// import { ViewPropertyComponent } from './view-property/view-property.component';
 import { AddPropertyComponent } from './add-property/add-property.component';
 import { EditPropertyComponent } from './edit-property/edit-property.component';
 
@@ -15,10 +15,10 @@ const routes: Routes = [
         path: '',
         component: PropertyManagementComponent
       },
-      {
-        path: 'view/:id',
-        component: ViewPropertyComponent
-      },
+      // {
+      //   path: 'view/:id',
+      //   component: ViewPropertyComponent
+      // },
       {
         path: 'add',
         component: AddPropertyComponent
