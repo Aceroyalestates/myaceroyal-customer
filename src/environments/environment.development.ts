735b534215234a423ee7d7368223e13b26749380
Daniel Obasuyi <danielobasuyi@gmail.com>
2026-03-10 06:56:48 +0100

    chore: update environment api urls

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,11 @@
 export const environment = {
-
   production: false,
 
   // baseUrl: 'http://onerevpayapi-dev.apps.non-core-dev.sterlingbank.com/api', // development
 
   baseUrl: 'http://onerevapi-qa.apps.non-core-dev.sterlingbank.com/api', // QA
-  apiUrl: 'https://myacero--myaceroyal-backend--bp8g7jxh9y8c.code.run/api',
+  // apiUrl: 'https://myacero--myaceroyal-backend--bp8g7jxh9y8c.code.run/api',
+  apiUrl: 'https://api.myaceroyal.com/',
 
   publicEncString: 'v2y/F?E(H+MbQegg',
   publicVector: 's4j/J?T$C+GQrEkl',
--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,15 +1,12 @@
 export const environment = {
   production: true,
 
-
-  // baseUrl: 'http://onerevpayapi-dev.apps.non-core-dev.sterlingbank.com/api', // development 
+  // baseUrl: 'http://onerevpayapi-dev.apps.non-core-dev.sterlingbank.com/api', // development
   baseUrl: 'http://onerevapi-qa.apps.non-core-dev.sterlingbank.com/api', // QA
+  apiUrl: 'https://api.myaceroyal.com/',
 
-
-  
- publicEncString: 'v2y/F?E(H+MbQegg',
+  publicEncString: 'v2y/F?E(H+MbQegg',
   publicVector: 's4j/J?T$C+GQrEkl',
 };
-
 
 // ng build -c uat // uat build command
